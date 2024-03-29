bcc1b1dfe05cf88a9e044399dcd1032d6908c262
sundarraj2003 <142962207+sundarraj2003@users.noreply.github.com>
2024-03-30 02:25:14 +0530

    Project sucess fully added

---
+++ Acsass/home/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Datas(models.Model):
+    Name=models.CharField(max_length=20,default="")
+    Age=models.IntegerField(default="")
+    Contact=models.CharField(max_length=20,default="")
+    Mail=models.CharField(max_length=50,default="")
